981ebebc8f39ff233ee54923b8aeb2d4fdc403ed
Oluwadipe Godwin Jesuropo <Gwinsworld@gmail.com>
2022-10-20 12:19:12 -0700

    --- Auto Git Commit ---

--- tal.js
+++ tal.js
@@ -1,47 +1,18 @@
-//Navigation handle 
 
-// let searchFrom = document.querySelector('.search-form');
+// let MenuBox = document.getElementsByClassName('menuBox');
+// let MenuBtn = document.getElementById('menu-btn');
+// let CLOSE = document.getElementById('close')
 
-// document.querySelector('#search-btn').onclick = () => {
-//     searchFrom.classList.toggle('active');
-//     cartPage.classList.remove('active');
-//     loginForm.classList.remove('active');
-//     navbar.classList.remove('active');
-// }
-
-// let navbar = document.querySelector('.navbar')
-// document.querySelector('#menu-btn').onclick = () => {
-//     navbar.classList.toggle('active');
-//     searchFrom.classList.remove('active');
-//     cartPage.classList.remove('active');
-//     loginForm.classList.remove('active');
-// }
-
-// window.onscroll = () => {
-//     navbar.classList.remove('active');
-//     searchFrom.classList.remove('active');
-//     cartPage.classList.remove('active');
-//     loginForm.classList.remove('active');
-// }
-
-// let arrowMove = document.getElementsByClassName('arrowMove')
-// let info = document.getElementsByClassName('info')
-
-let MenuBox = document.getElementsByClassName('menuBox');
-let MenuBtn = document.getElementById('menu-btn');
-let CLOSE = document.getElementById('close')
-
-MenuBtn.addEventListener('click', showUp)
-function showUp() {
-    if (MenuBtn.style.display === "none") {
-        MenuBox.style.display = "block"
-        CLOSE.style.display = "block"
-    }else {
-        MenuBtn.style.display = "block"
-        CLOSE.style.display = "none"
-        MenuBox.style.display = "none"
-    }
-}
+// MenuBtn.addEventListener('click', () => {
+//     if (MenuBtn.style.display === "none") {
+//         MenuBox.style.display = "block"
+//         CLOSE.style.display = "block"
+//     }else {
+//         MenuBtn.style.display = "block"
+//         CLOSE.style.display = "none"
+//         MenuBox.style.display = "none"
+//     }
+// })
 
 // search functionality On maxWidth
 // let SearchBlock = document.getElementsByClassName('search-form');
