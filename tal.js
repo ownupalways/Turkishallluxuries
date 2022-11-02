81b158fb5b17b590af1a8cb29dd5ba0e231c5c48
Oluwadipe Godwin Jesuropo <Gwinsworld@gmail.com>
2022-11-02 09:51:50 -0700

    --- Auto Git Commit ---

--- tal.js
+++ tal.js
@@ -1,11 +1,13 @@
 // Responsive MenuBox
 let menu = document.getElementsByClassName('navbar')[0];
+let MenuBox = document.getElementsByClassName('menubox')[0]
 let MenuBtn = document.getElementById('menu-btn');
 let closed = document.getElementById('close');
 
 MenuBtn.onclick = () => {
     menu.style.display = "block";
     menu.style.padding = "1px";
+    MenuBox.style.left = "0";
     closed.style.display ="block";
     MenuBtn.style.display = "none";
 }
@@ -13,5 +15,6 @@ MenuBtn.onclick = () => {
 closed.onclick = () => {
     menu.style.display = "none";
     closed.style.display ="none"
+ 
     MenuBtn.style.display = "block"
 }
