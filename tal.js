0495f0febda2a9d420a5029d617c35f4a2ead16f
Godwin Oluwadipe <gwinsworld@gmail.com>
2024-06-20 14:32:41 +0100

    Change image folder and also created Product.json

--- tal.js
+++ tal.js
@@ -1,3 +1,31 @@
+let productContentHTML = document.getElementsByClassName("ScrollTab")[0]
+console.log(productContentHTML)
+
+let scrollTabs = []
+
+const initApp = () => {
+    // get data from json
+    fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        scrollTabs = data;
+        console.log(scrollTabs)
+    })
+}
+initApp()
+
+function ready() {
+    let closeModalBoxDisplay = document.getElementsByClassName('closeMe')
+    closeModalBoxDisplay.forEach(thisCloseBtn => {
+        thisCloseBtn.addEventListener('click', removeModalBox)
+    });
+}
+
+function removeModalBox(event) {
+    let buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
+}
+
 // Responsive MenuBox
 let menu = document.getElementsByClassName('navbar')[0];
 let MenuBox = document.getElementsByClassName('menuBox')[0]
@@ -18,3 +46,59 @@ closed.onclick = () => {
     MenuBox.style.left = "-150px";
     MenuBtn.style.display = "block";
 }
+
+// MODAL BOX DISPLAY OPERATION
+
+// let viewBtn = document.getElementById('details')
+
+// let purchaseBtn = document.getElementById('purchase')
+
+
+// viewBtn.addEventListener('click', mobilePage)
+
+// function mobilePage() {
+//     let displayModalBox = document.createElement('div')
+//     displayModalBox.classList.add('modalBoxForEachImage')
+
+//     let Box = document.getElementsByClassName('rowOffIn')[0];
+   
+//    let myModalBox =`
+//     <div id = "modalBox">
+//         <div class="innerModalBox">
+//             <span><i class="fa-solid fa-xmark closeMe"></i></span>
+//             <div class="imageItem">
+//                 <img src="talsingle/single couch.jpg" alt="">
+//             </div>
+//             <div class="actionDecision">
+//                 <table>
+//                     <tr>
+//                         <td>Product Name:</td>
+//                         <td>Single Couch</td>
+//                     </tr>
+//                     <tr>
+//                         <td>Description:</td>
+//                         <td class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor amet iusto ab ut repellat ipsum nostrum. Temporibus sapiente molestiae fuga neque nostrum provident dolores culpa! Delectus ut minus hic consectetur!</td>
+//                     </tr>
+//                     <tr>
+//                         <td>Price:</td>
+//                         <td>$200</td>
+//                     </tr>
+//                     <tr>
+//                         <td></td>
+//                         <td class="btnContainer">
+//                             <button class="addToCart btn"> Add to Cart
+//                             </button>
+//                             <button class="purchaseNow btn">
+//                                 Purchase Now
+//                             </button>
+//                         </td>
+//                     </tr>
+//                 </table>
+//             </div>
+//         </div>
+//     </div>`;
+//     Box.append(displayModalBox)
+//     displayModalBox.innerHTML = myModalBox;
+
+//     displayModalBox.getElementsByClassName('closeMe')[0].addEventListener('click', removeModalBox )
+// }
